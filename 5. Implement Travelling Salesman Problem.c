bb22486b40517c3b8e4a32a1ecfecdbafcab05b2
11249a365-prog <11249a365@kanchiuniv.ac.in>
2026-04-19 12:44:39 +0530

    Implement Travelling Salesman Problem algorithm

---
+++ 5. Implement Travelling Salesman Problem.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <limits.h>
+
+int cost[10][10], visited[10], n;
+int minCost = INT_MAX;
+
+void tsp(int city, int count, int currentCost) {
+    if (count == n && cost[city][0] > 0) {
+        currentCost += cost[city][0];
+        if (currentCost < minCost)
+            minCost = currentCost;
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i] && cost[city][i] > 0) {
+            visited[i] = 1;
+            tsp(i, count + 1, currentCost + cost[city][i]);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main() {
+    printf("Enter number of cities: ");
+    scanf("%d", &n);
+
+    printf("Enter cost matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &cost[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    visited[0] = 1;
+    tsp(0, 1, 0);
+
+    printf("Minimum cost = %d\n", minCost);
+
+    return 0;
+}
